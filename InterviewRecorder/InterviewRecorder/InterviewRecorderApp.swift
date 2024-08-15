ff3f2554b8d9d8c383ed2d2a7ac09cbfa23156f8
Wooseok Lee <wl00ie19@gmail.com>
2024-08-15 15:54:10 +0900

    feat: 최초 프로젝트 생성

---
+++ InterviewRecorder/InterviewRecorder/InterviewRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewRecorderApp.swift
+//  InterviewRecorder
+//
+//  Created by 이우석 on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct InterviewRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
