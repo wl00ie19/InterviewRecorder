9924124ae0ac2c30c5c015516d9620e126c43d28
Wooseok Lee <wl00ie19@gmail.com>
2024-08-15 17:57:22 +0900

    feat: 질문 모델 녹음 파일 길이 추가, 모델 init 시 옵셔널 변수는 nil로 지정

--- InterviewRecorder/InterviewRecorder/Models/Question.swift
+++ InterviewRecorder/InterviewRecorder/Models/Question.swift
@@ -22,6 +22,9 @@ class Question {
     /// 답변 녹음 파일 URL
     let answerURLString: String?
     
+    /// 답변 녹음 파일 길이
+    let answerLength: Double?
+    
     /// 마지막 답변 일자
     let lastAnsweredDate: Date?
     
@@ -30,11 +33,9 @@ class Question {
         answerURLString != nil
     }
     
-    init(id: UUID, content: String, questionDate: Date, answerURLString: String?, lastAnsweredDate: Date?) {
+    init(id: UUID, content: String, questionDate: Date) {
         self.id = id
         self.content = content
         self.questionDate = questionDate
-        self.answerURLString = answerURLString
-        self.lastAnsweredDate = lastAnsweredDate
     }
 }
