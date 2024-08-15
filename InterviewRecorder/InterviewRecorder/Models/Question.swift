c59940a7d8ff03267b463c856d1373d27f746b88
Wooseok Lee <wl00ie19@gmail.com>
2024-08-15 17:15:05 +0900

    feat: 면접 질문 모델 누락 변수 추가 및 주석 추가

--- InterviewRecorder/InterviewRecorder/Models/Question.swift
+++ InterviewRecorder/InterviewRecorder/Models/Question.swift
@@ -10,13 +10,29 @@ import SwiftData
 
 @Model
 class Question {
+    /// 질문 ID
     let id: UUID
+    
+    /// 질문 내용
+    let content: String
+    
+    /// 질문 등록 일자
     let questionDate: Date
+    
+    /// 답변 녹음 파일 URL
     let answerURLString: String?
+    
+    /// 마지막 답변 일자
     let lastAnsweredDate: Date?
     
-    init(id: UUID, questionDate: Date, answerURLString: String?, lastAnsweredDate: Date?) {
+    /// 답변 여부
+    var isAnswered: Bool {
+        answerURLString != nil
+    }
+    
+    init(id: UUID, content: String, questionDate: Date, answerURLString: String?, lastAnsweredDate: Date?) {
         self.id = id
+        self.content = content
         self.questionDate = questionDate
         self.answerURLString = answerURLString
         self.lastAnsweredDate = lastAnsweredDate
