404b16c9f311c6999fd81a3bff70421970c9473b
Wooseok Lee <wl00ie19@gmail.com>
2024-08-27 11:56:11 +0900

    feat: 기본(질문 목록) 뷰 간격 수정

--- InterviewRecorder/InterviewRecorder/ContentView.swift
+++ InterviewRecorder/InterviewRecorder/ContentView.swift
@@ -14,30 +14,33 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVStack(spacing: 20) {
-                    VStack(spacing: 30) {
-                        Text("답변하지 않은 질문")
-                            .font(.headline)
-                        
-                        Text("EnvironmentObject는 무엇이고 어떤 경우 사용할까요?")
-                            .font(.title)
-                        
-                        ColorButton(title: "답변하기", buttonColor: .blue, textColor: Color(uiColor: .systemBackground)) {
+                LazyVStack(spacing: 0) {
+                    VStack(spacing: 10) {
+                        VStack(spacing: 30) {
+                            Text("답변하지 않은 질문")
+                                .font(.headline)
                             
+                            Text("EnvironmentObject는 무엇이고 어떤 경우 사용할까요?")
+                                .font(.title)
+                            
+                            ColorButton(title: "답변하기", buttonColor: .blue, textColor: Color(uiColor: .systemBackground)) {
+                                
+                            }
                         }
+                        .padding(10)
+                        .background {
+                            RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                                .strokeBorder(Color(uiColor: .systemGray3), lineWidth: 2)
+                        }
+                        
+                        ColorButton(title: "질문 추가하기", buttonColor: Color(uiColor: .systemGray5)) {
+                            isShowingNewQuestion.toggle()
+                        }
+                        .padding(.horizontal, 10)
                     }
                     .padding(10)
-                    .background {
-                        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-                            .strokeBorder(Color(uiColor: .systemGray3), lineWidth: 2)
-                    }
-                    
-                    ColorButton(title: "질문 추가하기", buttonColor: Color(uiColor: .systemGray5)) {
-                        isShowingNewQuestion.toggle()
-                    }
-                    .padding(.horizontal, 10)
                 }
-                .padding(10)
+                .padding(.vertical, 10)
             }
             .navigationTitle("전체 답변 목록")
             .toolbar {
