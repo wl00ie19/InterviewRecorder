93767f1dd934fef1b13cf13b180518cc38623362
Wooseok Lee <wl00ie19@gmail.com>
2024-08-31 23:47:02 +0900

    feat: 면접 답변 화면 간격 조정, 질문 길어지면 작아지도록 처리

--- InterviewRecorder/InterviewRecorder/AnswerRecordingView.swift
+++ InterviewRecorder/InterviewRecorder/AnswerRecordingView.swift
@@ -20,57 +20,61 @@ struct AnswerRecordingView: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading, spacing: 20) {
-                    Text("질문: ")
-                        .font(.headline)
-                    
-                    Text(question.content)
-                        .font(.title)
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("질문: ")
+                            .font(.headline)
+                        
+                        Text(question.content)
+                            .font(.title)
+                            .minimumScaleFactor(0.3)
+                    }
                 }
+                .padding(.bottom, 30)
+                
+                if question.isAnswered {
+                    PlayButton(question: $question) {
+                        switch recordManager.status {
+                        case .stop:
+                            if let fileName = question.answerFileName {
+                                recordManager.startPlay(fileName: fileName)
+                            }
+                        case .play:
+                            recordManager.pausePlay()
+                        case .pause:
+                            recordManager.resumePlay()
+                        case .record:
+                            break
+                        }
+                    }
+                    .frame(height: 80)
+                    .disabled(isRecording)
+                } else {
+                    RecordVolumeGauge()
+                        .frame(height: 80)
+                }
+                
+                ColorButton(title: isRecording ? "정지" : "녹음하기" , buttonColor: isRecording ? .nowRecording : .recordButton, textColor: Color(uiColor: .systemBackground)) {
+                    if isRecording {
+                        let (answerURLString, length) = recordManager.stopRecord()
+                        
+                        question.answerFileName = answerURLString
+                        question.answerLength = length
+                        question.lastAnsweredDate = Date()
+                        
+                        modelContext.insert(question)
+                    } else {
+                        recordManager.startRecord(questionID: question.id)
+                    }
+                }
+                .disabled(question.isAnswered)
                 
                 Spacer()
             }
-            .padding(.bottom, 30)
-            
-            if question.isAnswered {
-                PlayButton(question: $question) {
-                    switch recordManager.status {
-                    case .stop:
-                        if let fileName = question.answerFileName {
-                            recordManager.startPlay(fileName: fileName)
-                        }
-                    case .play:
-                        recordManager.pausePlay()
-                    case .pause:
-                        recordManager.resumePlay()
-                    case .record:
-                        break
-                    }
-                }
-                .frame(height: 80)
-                .disabled(isRecording)
-            } else {
-                RecordVolumeGauge()
-                    .frame(height: 80)
-            }
-            
-            ColorButton(title: isRecording ? "정지" : "녹음하기" , buttonColor: isRecording ? .nowRecording : .recordButton, textColor: Color(uiColor: .systemBackground)) {
-                if isRecording {
-                    let (answerURLString, length) = recordManager.stopRecord()
-                    
-                    question.answerFileName = answerURLString
-                    question.answerLength = length
-                    question.lastAnsweredDate = Date()
-                    
-                    modelContext.insert(question)
-                } else {
-                    recordManager.startRecord(questionID: question.id)
-                }
-            }
-            .disabled(question.isAnswered)
+            .padding(10)
         }
-        .padding(10)
+        .toolbarTitleDisplayMode(.inline)
     }
 }
