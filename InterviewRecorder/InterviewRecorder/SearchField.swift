c3309f485d6d15b4b733e81a08217e320499fe1c
Wooseok Lee <wl00ie19@gmail.com>
2024-09-26 23:53:16 +0900

    feat: 검색창 뷰 추가

---
+++ InterviewRecorder/InterviewRecorder/SearchField.swift
@@ -0,0 +1,33 @@
+//
+//  SearchField.swift
+//  InterviewRecorder
+//
+//  Created by 이우석 on 9/26/24.
+//
+
+import SwiftUI
+
+struct SearchField: View {
+    @Binding var searchText: String
+    
+    @FocusState private var focused: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            TextField("검색", text: $searchText)
+                .focused($focused)
+                .font(.title3)
+            
+            Rectangle()
+                .frame(height: 2)
+                .foregroundStyle(focused ? Color(uiColor: .systemGray3) : Color(uiColor: .systemGray5))
+        }
+        .padding(10)
+        .animation(.easeIn(duration: 0.15), value: focused)
+        .onAppear{
+            #if canImport(UIKit)
+            UITextField.appearance().clearButtonMode = .whileEditing
+            #endif
+        }
+    }
+}
