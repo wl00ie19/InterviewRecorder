792fb835a29124bdc3605baea0b643d0a07a77a5
Wooseok Lee <wl00ie19@gmail.com>
2024-10-11 09:53:17 +0900

    feat: 답변하지 않은 질문 필터링 뷰 추가

--- InterviewRecorder/InterviewRecorder/SearchField.swift
+++ InterviewRecorder/InterviewRecorder/SearchField.swift
@@ -10,19 +10,41 @@ import SwiftUI
 struct SearchField: View {
     @Binding var searchText: String
     
+    @State var isUnansweredOnly: Bool = false
+    
     @FocusState private var focused: Bool
     
+    var checkIcon: String {
+        isUnansweredOnly ? "checkmark.circle.fill" : "checkmark.circle"
+    }
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            TextField("검색", text: $searchText)
-                .focused($focused)
+        VStack {
+            VStack(alignment: .leading, spacing: 5) {
+                TextField("검색", text: $searchText)
+                    .focused($focused)
+                
+                Rectangle()
+                    .frame(height: 2)
+                    .foregroundStyle(focused ? Color(uiColor: .systemGray3) : Color(uiColor: .systemGray5))
+                
+            }
+            .animation(.easeIn(duration: 0.15), value: focused)
             
-            Rectangle()
-                .frame(height: 2)
-                .foregroundStyle(focused ? Color(uiColor: .systemGray3) : Color(uiColor: .systemGray5))
+            Button {
+                isUnansweredOnly.toggle()
+            } label: {
+                Label("답변하지 않은 질문만 보기", systemImage: checkIcon)
+                    .foregroundStyle(isUnansweredOnly ? .white : .primary)
+                    .padding(10)
+                    .background {
+                        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                            .foregroundStyle(isUnansweredOnly ? .blue : Color(uiColor: .systemGray5))
+                    }
+            }
+            .buttonStyle(CustomButtonStyle())
         }
         .padding(10)
-        .animation(.easeIn(duration: 0.15), value: focused)
         .onAppear{
             #if canImport(UIKit)
             UITextField.appearance().clearButtonMode = .whileEditing
